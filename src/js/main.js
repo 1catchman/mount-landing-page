5ebacd793297e3b98ffe77d64872e590897beddf
1catchman <msgforcatchman@gmail.com>
2022-01-01 19:54:30 +0600

    navbar mobile menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
 (function ($) {
-  'use strict';
+  "use strict";
 
   /**
    * Easy selector helper function
@@ -30,14 +30,21 @@
   /**
    * Easy on scroll event listener
    */
+  const onclick = (el, listener) => {
+    el.addEventListener("click", listener);
+  };
+
+  /**
+   * Easy on scroll event listener
+   */
   const onscroll = (el, listener) => {
-    el.addEventListener('scroll', listener);
+    el.addEventListener("scroll", listener);
   };
 
   /**
    * Navbar links active state on scroll
    */
-  let navbarlinks = select('#navbar .scrollto', true);
+  let navbarlinks = select("#navbar .scrollto", true);
   const navbarlinksActive = () => {
     let position = window.scrollY + 200;
     navbarlinks.forEach((navbarlink) => {
@@ -48,26 +55,40 @@
         position >= section.offsetTop &&
         position <= section.offsetTop + section.offsetHeight
       ) {
-        navbarlink.classList.add('menu-active');
+        navbarlink.classList.add("menu-active");
       } else {
-        navbarlink.classList.remove('menu-active');
+        navbarlink.classList.remove("menu-active");
       }
     });
   };
-  window.addEventListener('load', navbarlinksActive);
+  window.addEventListener("load", navbarlinksActive);
   onscroll(document, navbarlinksActive);
+
+  /**
+   * Navbar mobile links menu open/close on click
+   */
+  let menuBtn = select(".menu-btn");
+  const navMobileLinksToggle = () => {
+    let header = select(".header");
+    let headerMenu = select(".header__menu");
+    if (menuBtn && header && headerMenu) {
+      menuBtn.classList.toggle("menu-btn-active");
+      header.classList.toggle("navbar-active__header");
+      headerMenu.classList.toggle("navbar-active__menu");
+    }
+  };
+  onclick(menuBtn, navMobileLinksToggle);
 
   /**
    * Scrolls to an element with header offset
    */
   const scrollto = (el) => {
-    let header = select('#header');
+    let header = select("#header");
     let offset = header.offsetHeight;
-
     let elementPos = select(el).offsetTop;
     window.scrollTo({
       top: elementPos + offset,
-      behavior: 'smooth'
+      behavior: "smooth",
     });
   };
 
@@ -75,19 +96,13 @@
    * Scrool with ofset on links with a class name .scrollto
    */
   on(
-    'click',
-    '.scrollto',
+    "click",
+    ".scrollto",
     function (e) {
       if (select(this.hash)) {
         e.preventDefault();
-
-        let navbar = select('#navbar');
-        if (navbar.classList.contains('navbar-mobile')) {
-          navbar.classList.remove('navbar-mobile');
-          let navbarToggle = select('.mobile-nav-toggle');
-          navbarToggle.classList.toggle('bi-list');
-          navbarToggle.classList.toggle('bi-x');
-        }
+        if (menuBtn.classList.contains("menu-btn-active"))
+          navMobileLinksToggle();
         scrollto(this.hash);
       }
     },
@@ -97,7 +112,7 @@
   /**
    * Scroll with ofset on page load with hash links in the url
    */
-  window.addEventListener('load', () => {
+  window.addEventListener("load", () => {
     if (window.location.hash) {
       if (select(window.location.hash)) {
         scrollto(window.location.hash);
@@ -108,116 +123,116 @@
   /**
    * Preloader
    */
-  let preloader = select('#preloader');
+  let preloader = select("#preloader");
   if (preloader) {
-    window.addEventListener('load', () => {
+    window.addEventListener("load", () => {
       preloader.remove();
     });
   }
 
   // Porfolio isotope and filter
-  $(window).on('load', function () {
-    var portfolioIsotope = $('.image-list').isotope({
-      itemSelector: '.image-list-item'
+  $(window).on("load", function () {
+    var portfolioIsotope = $(".image-list").isotope({
+      itemSelector: ".image-list-item",
     });
 
-    $('#portfolio-flters li').on('click', function () {
-      $('#portfolio-flters li').removeClass('filter-active');
-      $(this).addClass('filter-active');
+    $("#portfolio-flters li").on("click", function () {
+      $("#portfolio-flters li").removeClass("filter-active");
+      $(this).addClass("filter-active");
 
       portfolioIsotope.isotope({
-        filter: $(this).data('filter')
+        filter: $(this).data("filter"),
       });
       aos_init();
     });
 
     // Initiate venobox (lightbox feature used in portfolio)
     $(document).ready(function () {
-      $('.venobox').venobox();
+      $(".venobox").venobox();
     });
   });
 
   // Portfolio details carousel
-  $('.portfolio-details-carousel').owlCarousel({
+  $(".portfolio-details-carousel").owlCarousel({
     autoplay: true,
     dots: true,
     loop: true,
-    items: 1
+    items: 1,
   });
 
   // Init AOS
   function aos_init() {
     AOS.init({
       duration: 1000,
-      easing: 'ease-in-out',
+      easing: "ease-in-out",
       once: true,
-      mirror: false
+      mirror: false,
     });
   }
-  $(window).on('load', function () {
+  $(window).on("load", function () {
     aos_init();
   });
 
   // Contact Form
 
   var contactForm = function () {
-    if ($('#contactForm').length > 0) {
-      $('#contactForm').validate({
+    if ($("#contactForm").length > 0) {
+      $("#contactForm").validate({
         rules: {
-          fname: 'required',
-          lname: 'required',
+          fname: "required",
+          lname: "required",
           email: {
             required: true,
-            email: true
+            email: true,
           },
           message: {
             required: true,
-            minlength: 5
-          }
+            minlength: 5,
+          },
         },
         messages: {
-          fname: 'Please enter your first name',
-          lname: 'Please enter your last name',
-          email: 'Please enter a valid email address',
-          message: 'Please enter a message'
+          fname: "Please enter your first name",
+          lname: "Please enter your last name",
+          email: "Please enter a valid email address",
+          message: "Please enter a message",
         },
         /* submit via ajax */
         submitHandler: function (form) {
-          var $submit = $('.submitting'),
-            waitText = 'Submitting...';
+          var $submit = $(".submitting"),
+            waitText = "Submitting...";
 
           $.ajax({
-            type: 'POST',
-            url: 'php/send-email.php',
+            type: "POST",
+            url: "php/send-email.php",
             data: $(form).serialize(),
 
             beforeSend: function () {
-              $submit.css('display', 'block').text(waitText);
+              $submit.css("display", "block").text(waitText);
             },
             success: function (msg) {
-              if (msg == 'OK') {
-                $('#form-message-warning').hide();
+              if (msg == "OK") {
+                $("#form-message-warning").hide();
                 setTimeout(function () {
-                  $('#contactForm').fadeOut();
+                  $("#contactForm").fadeOut();
                 }, 1000);
                 setTimeout(function () {
-                  $('#form-message-success').fadeIn();
+                  $("#form-message-success").fadeIn();
                 }, 1400);
               } else {
-                $('#form-message-warning').html(msg);
-                $('#form-message-warning').fadeIn();
-                $submit.css('display', 'none');
+                $("#form-message-warning").html(msg);
+                $("#form-message-warning").fadeIn();
+                $submit.css("display", "none");
               }
             },
             error: function () {
-              $('#form-message-warning').html(
-                'Something went wrong. Please try again.'
+              $("#form-message-warning").html(
+                "Something went wrong. Please try again."
               );
-              $('#form-message-warning').fadeIn();
-              $submit.css('display', 'none');
-            }
+              $("#form-message-warning").fadeIn();
+              $submit.css("display", "none");
+            },
           });
-        }
+        },
       });
     }
   };
